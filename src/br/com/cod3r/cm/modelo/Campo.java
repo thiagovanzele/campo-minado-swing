e8827d3a8f231a517b517860b13bd87557b4d96c
Thiago Vanzele <thiagovanzele@gmail.com>
2023-08-25 19:33:05 -0300

    Refatorando classes de excessao

--- src/br/com/cod3r/cm/modelo/Campo.java
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -3,8 +3,6 @@ package br.com.cod3r.cm.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.com.cod3r.cm.excecao.ExplosaoException;
-
 public class Campo {
 
 	private final int linha;
@@ -52,7 +50,7 @@ public class Campo {
 			aberto = true;
 
 			if (minado) {
-				throw new ExplosaoException();
+				//TODO implementar nova versão
 			}
 			if (vizinhancaSegura()) {
 				vizinhos.forEach(v -> v.abrir());
@@ -115,18 +113,5 @@ public class Campo {
 		marcado = false;
 	}
 	
-	public String toString() {
-		if (marcado) {
-			return "x";
-		} else if (aberto && minado) {
-			return "*";
-		} else if (aberto && minasNaVizinhanca() > 0) {
-			return Long.toString(minasNaVizinhanca());
-		} else if (aberto) {
-			return " ";
-		} else {
-			return "?";
-		}
-	}
 	
 }
