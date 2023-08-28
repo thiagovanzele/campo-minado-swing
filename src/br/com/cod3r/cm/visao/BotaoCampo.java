764bd83223fc3e228fcade7685f55f9da30c88ec
Thiago Vanzele <thiagovanzele@gmail.com>
2023-08-28 18:16:08 -0300

    Corrigindo bug de não abrir campos

--- src/br/com/cod3r/cm/visao/BotaoCampo.java
+++ src/br/com/cod3r/cm/visao/BotaoCampo.java
@@ -6,18 +6,16 @@ import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.SwingUtilities;
 
 import br.com.cod3r.cm.modelo.Campo;
 import br.com.cod3r.cm.modelo.CampoEvento;
 import br.com.cod3r.cm.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton 
-	implements CampoObservador, MouseListener {
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener {
 
 	private final Color BG_PADRAO = new Color(184, 184, 184);
-	private final Color BG_MARCAR = new Color(8, 179, 247);
+	private final Color BG_MARCADO = new Color(8, 179, 247);
 	private final Color BG_EXPLODIR = new Color(189, 66, 68);
 	private final Color TEXTO_VERDE = new Color(0, 100, 0);
 	
@@ -26,14 +24,13 @@ public class BotaoCampo extends JButton
 	public BotaoCampo(Campo campo) {
 		this.campo = campo;
 		setBackground(BG_PADRAO);
-		setOpaque(true);
 		setBorder(BorderFactory.createBevelBorder(0));
-		
 		
 		addMouseListener(this);
 		campo.registrarObservador(this);
+		
 	}
-	
+
 	@Override
 	public void eventoOcorreu(Campo campo, CampoEvento evento) {
 		switch(evento) {
@@ -49,44 +46,29 @@ public class BotaoCampo extends JButton
 		default:
 			aplicarEstiloPadrao();
 		}
-		
-		SwingUtilities.invokeLater(() -> {
-			repaint();
-			validate();
-		});
 	}
 
 	private void aplicarEstiloPadrao() {
-		setBackground(BG_PADRAO);
-		setBorder(BorderFactory.createBevelBorder(0));
-		setText("");
+		// TODO Auto-generated method stub
+		
 	}
 
 	private void aplicarEstiloExplodir() {
-		setBackground(BG_EXPLODIR);
-		setForeground(Color.WHITE);
-		setText("X");
+		// TODO Auto-generated method stub
+		
 	}
 
 	private void aplicarEstiloMarcar() {
-		setBackground(BG_MARCAR);
-		setForeground(Color.BLACK);
-		setText("M");
+		// TODO Auto-generated method stub
+		
 	}
 
 	private void aplicarEstiloAbrir() {
-		
-		setBorder(BorderFactory.createLineBorder(Color.GRAY));
-
-		if(campo.isMinado()) {
-			setBackground(BG_EXPLODIR);
-			return;
-		}
-		
 		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createLineBorder(Color.GRAY));
 		
 		switch (campo.minasNaVizinhanca()) {
-		case 1:
+		case 1: 
 			setForeground(TEXTO_VERDE);
 			break;
 		case 2:
@@ -104,15 +86,16 @@ public class BotaoCampo extends JButton
 			setForeground(Color.PINK);
 		}
 		
-		String valor = !campo.vizinhancaSegura() ? 
+		String valor = !campo.vizinhancaSegura() ?
 				campo.minasNaVizinhanca() + "" : "";
 		setText(valor);
+		
 	}
-	
-	// Interface dos eventos do Mouse
+
 	@Override
 	public void mousePressed(MouseEvent e) {
-		if(e.getButton() == 1) {
+
+		if (e.getButton() == 1) {
 			campo.abrir();
 		} else {
 			campo.alternarMarcacao();
@@ -120,7 +103,7 @@ public class BotaoCampo extends JButton
 	}
 	
 	public void mouseClicked(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
 	public void mouseEntered(MouseEvent e) {}
 	public void mouseExited(MouseEvent e) {}
-	public void mouseReleased(MouseEvent e) {}
 }
